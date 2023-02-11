84b02517f04bde3106e38678cd5e40b6fba93fe9
sjoshi91119 <115713904+sjoshi91119@users.noreply.github.com>
2023-02-11 19:35:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,53 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  //Write your code here , just console.
+  
+ arr.map(obj => {
+    if(obj.marks > 50){
+      console.log(obj);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  
+   arr.forEach(element => {
+    if(element.marks > 50){
+      console.log(element);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  
+   let obj = {id:4,name:"susan",age:"20",marks:45};
+  arr.push(obj);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  
+  for(let i = 0; i <= arr.length; i++){
+    if(arr[i].marks <= 50){
+      arr.splice(i, i + 1);
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  
+   let ids = arr.length;
+  let arr1 = [
+    { id: ids + 1, name: "Shubham", age: "21", marks: 89 },
+    { id: ids + 2, name: "Dinesh", age: "28", marks: 82 },
+    { id: ids + 3, name: "Rahul", age: "24", marks: 43 },
+  ];
+  let array = arr.concat(arr1);
+  console.log(array)
 }
